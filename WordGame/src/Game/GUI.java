01cd5e1ae4c58e4f2266b52225a5e8dccfa6fd6e
Xavier Oyola <53361208+paperbag34@users.noreply.github.com>
2023-08-19 21:04:55 -0700

    Fix Screen size

--- WordGame/src/Game/GUI.java
+++ WordGame/src/Game/GUI.java
@@ -31,7 +31,7 @@ public class GUI {
         
     	frame = new JFrame("Word Guessing Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1200, 1000);
 
         panel = new JPanel();
         panel.setLayout(new BorderLayout());
